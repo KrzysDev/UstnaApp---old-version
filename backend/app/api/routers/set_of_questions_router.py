62ebdcbb17da485cb93c7bcc3f2a672ee5642d10
KrzysDev <krzysiekk.sokolowski@gmail.com>
2026-06-07 15:28:00 +0200

    feat: added pictures for interpretation and some minor changes to set_of_questions_router

--- backend/app/api/routers/set_of_questions_router.py
+++ backend/app/api/routers/set_of_questions_router.py
@@ -1,17 +1,30 @@
 from fastapi import APIRouter
 import random
-
+import json
+from app.models.schemas import Question, SetOfQuestionsResponse
 router = APIRouter(prefix="/set-of-questions", tags=["set-of-questions"])
 
 
 @router.get("/")
-def get_random_set_of_questions():
+def get_random_set_of_questions() -> SetOfQuestionsResponse:
     with open("app/data/questions.txt", "r", encoding="utf-8") as file:
         questions = file.read().splitlines()
     
-    random_question = questions[random.randint(1, len(questions))]
-    
-    return {
-        "question1" : random_question,
-        "question2" : questions[random.randint(1, len(questions))]
-    }
+    public_question = questions[random.randint(0, len(questions)-1)]
+    with open("app/data/questions/secret_questions.json", "r", encoding="utf-8") as file:
+        secret_questions = json.load(file)
+
+    secret_question_index = random.randint(0, len(secret_questions)-1)
+    secret_question = secret_questions[secret_question_index]
+
+    return SetOfQuestionsResponse(
+        question1=Question(
+            question=public_question,
+            question_type="text"
+        ),
+        question2=Question(
+            question=secret_question["question"],
+            question_type="image",
+            image_path=secret_question['image_path']
+        )
+    )
--- backend/app/models/schemas.py
+++ backend/app/models/schemas.py
@@ -1,6 +1,8 @@
 # pyrefly: ignore [missing-import]
 
 from pydantic import BaseModel
+from typing import Literal
+
 
 class EvaluationRequest(BaseModel):
     examination_board_question1: str
@@ -11,3 +13,14 @@ class EvaluationRequest(BaseModel):
     response1: str
     question2: str
     response2: str
+
+
+class Question(BaseModel):
+    question: str
+    question_type: Literal["text", "image"]
+    image_path: str = None
+
+
+class SetOfQuestionsResponse(BaseModel):
+    question1: Question
+    question2: Question
